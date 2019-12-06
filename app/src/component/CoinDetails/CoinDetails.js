76ca8e1191c22cf3ec4393bee4136b419beeeabb
Eric Atento <eric.atento@boom.camp>
2019-12-06 17:36:58 +0800

    initial graph design

--- app/src/component/CoinDetails/CoinDetails.js
+++ app/src/component/CoinDetails/CoinDetails.js
@@ -1,44 +1,49 @@
-import React, { useState, useEffect } from "react"
-import axios from "axios"
-import styled from "styled-components"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import Chart from "../chart.js";
 
 const Main = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   background-image: linear-gradient(green, yellow);
   height: 100vh;
-`
+`;
 const Container = styled.div`
   display: flex;
   justify-content: space-around;
-  margin-top: 80px;
-  width: 70%;
-  border: 2px solid black;
+  margin-top: 50px;
+  width: 90%;
   padding: 20px;
-  background: whitesmoke;
-`
+`;
 const Loading = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
-`
+`;
+const Btn = styled.button`
+  border: 1px solid black;
+  background: transparent;
+  border-radius: 5px;
+`;
 export default function CoinDetails({ match }) {
-  const [loading, setLoading] = useState(true)
-  const [details, setDetails] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [details, setDetails] = useState([]);
 
   useEffect(() => {
     axios
       .get(`https://api.coingecko.com/api/v3/coins/${match.params.id}`)
       .then(res => {
-        console.log(res.data)
-        setDetails(res.data)
+        console.log(res.data);
+        setDetails(res.data);
         setTimeout(() => {
-          setLoading(false)
-        }, 1000)
-      })
-  }, [])
+          setLoading(false);
+        }, 1000);
+      });
+  }, []);
   return (
     <>
       {loading ? (
@@ -47,13 +52,23 @@ export default function CoinDetails({ match }) {
         <Main>
           <Container>
             <img
-              style={{ margin: "20px 20px" }}
+              style={{ margin: "20px 20px", width: "50%" }}
               src={details.image.large}
             ></img>
             <div>{details.description.en}</div>
           </Container>
+          <Container style={{ flexDirection: "column" }}>
+            <div style={{ marginLeft: "86%", paddingBottom: 20 }}>
+              <Btn>24hr</Btn>
+              <Btn>30d</Btn>
+              <Btn>6m</Btn>
+              <Btn>1y</Btn>
+              <Btn>Max</Btn>
+            </div>
+            <Chart />
+          </Container>
         </Main>
       )}
     </>
-  )
+  );
 }
--- app/src/component/CoinList/CoinList.js
+++ app/src/component/CoinList/CoinList.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from "react"
-import axios from "axios"
-import { Grid } from "@material-ui/core"
-import { makeStyles } from "@material-ui/core/styles"
-import Table from "@material-ui/core/Table"
-import TableBody from "@material-ui/core/TableBody"
-import TableCell from "@material-ui/core/TableCell"
-import TableHead from "@material-ui/core/TableHead"
-import TableRow from "@material-ui/core/TableRow"
-import Paper from "@material-ui/core/Paper"
-import "semantic-ui-css/semantic.min.css"
-import { Pagination } from "semantic-ui-react"
-import styled from "styled-components"
-import background from "../../img/background.jpg"
- 
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import "semantic-ui-css/semantic.min.css";
+import { Pagination } from "semantic-ui-react";
+import styled from "styled-components";
+import background from "../../img/background.jpg";
+
 const useStyles = makeStyles({
   root: {
     width: "100%",
@@ -21,11 +21,11 @@ const useStyles = makeStyles({
     backgroundSize: "cover"
   },
   table: {
-    maxWidth: '95%',
+    maxWidth: "95%",
     background: "white",
-    opacity: '0.9'
+    opacity: "0.9"
   }
-})
+});
 const Header = styled.div`
   background: blue;
   display: flex;
@@ -33,24 +33,24 @@ const Header = styled.div`
   justify-content: center;
   height: 70px;
   margin-bottom: 20px;
-  text-Transform: uppercase;
-  color: white; 
+  text-transform: uppercase;
+  color: white;
   font-size: 60px;
-`
+`;
 
 const HeadTable = styled(TableHead)`
-  background: skyblue; 
-`
+  background: skyblue;
+`;
 const TableContainer = styled.div`
   display: flex;
   justify-content: center;
-`
+`;
 export default function SimpleTable(props) {
-  const classes = useStyles()
-  const [rows, setRows] = useState([])
-  const [cpage, setPage] = useState(1)
-  const [num, setNum] = useState(0)
-  const url = "https://api.coingecko.com/api/v3/"
+  const classes = useStyles();
+  const [rows, setRows] = useState([]);
+  const [cpage, setPage] = useState(1);
+  const [num, setNum] = useState(0);
+  const url = "https://api.coingecko.com/api/v3/";
 
   useEffect(() => {
     axios
@@ -58,78 +58,85 @@ export default function SimpleTable(props) {
         `${url}coins/markets?vs_currency=usd&order=market_cap_desc&per_page=14&page=${cpage}&sparkline=false`
       )
       .then(res => {
-        setRows([...res.data])
+        setRows([...res.data]);
+        console.log(res.data);
       })
-      .catch(err => console.log(err))
+      .catch(err => console.log(err));
     axios.get(`${url}coins/list`).then(res => {
-      setNum(Math.ceil((res.data.length + 1) / 14))
-    })
-  }, [cpage])
+      setNum(Math.ceil((res.data.length + 1) / 14));
+    });
+  }, [cpage]);
   const changePage = (e, pageInfo) => {
-    setPage(pageInfo.activePage)
-  }
+    setPage(pageInfo.activePage);
+  };
   return (
-    
     <Paper className={classes.root}>
       <Header>Cryptocurrencies</Header>
       <TableContainer>
-      <Table className={classes.table} aria-label="simple table">
-        <HeadTable>
-          <TableRow>
-            <TableCell>#</TableCell>
-            <TableCell>Coin</TableCell>
-            <TableCell>Symbol</TableCell>
-            <TableCell>Current Price</TableCell>
-            <TableCell>24h Volume</TableCell>
-            <TableCell>Circulating Supply</TableCell>
-            <TableCell>Mkt Cap</TableCell>
-          </TableRow>
-        </HeadTable>
-        <TableBody>
-          {rows.map(row => (
-            <TableRow key={row.name}>
-              <TableCell>{row.market_cap_rank}</TableCell>
-              <TableCell
-                style={{ display: "flex", alignItems: "center", cursor: "pointer"}}
-                onClick={() => props.history.push(`/coindetails/${row.id}`)}
-              >
-                <img
-                  style={{ width: 20, height: 20, marginRight: 20 }}
-                  src={`${row.image}`}
-                  alt="logo"
-                ></img>
-                {row.name}
-              </TableCell>
-              <TableCell style={{ textTransform: "uppercase" }}>
-                {row.symbol}
-              </TableCell>
-              <TableCell>{row.current_price}</TableCell>
-              <TableCell>
-                {row.total_volume
-                  ? `$${row.total_volume.toLocaleString()}`
-                  : " "}
-              </TableCell>
-              <TableCell>
-                {row.circulating_supply
-                  ? row.circulating_supply.toLocaleString()
-                  : " "}
-              </TableCell>
-              <TableCell>
-                {row.market_cap ? `$${row.market_cap.toLocaleString()}` : " "}
-              </TableCell>
+        <Table className={classes.table} aria-label="simple table">
+          <HeadTable>
+            <TableRow>
+              <TableCell>#</TableCell>
+              <TableCell>Coin</TableCell>
+              <TableCell>Symbol</TableCell>
+              <TableCell>Current Price</TableCell>
+              <TableCell>24h Volume</TableCell>
+              <TableCell>Circulating Supply</TableCell>
+              <TableCell>Mkt Cap</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </HeadTable>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.name}>
+                <TableCell>{row.market_cap_rank}</TableCell>
+                <TableCell
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    cursor: "pointer"
+                  }}
+                  onClick={() => props.history.push(`/coindetails/${row.id}`)}
+                >
+                  <img
+                    style={{ width: 20, height: 20, marginRight: 20 }}
+                    src={`${row.image}`}
+                    alt="logo"
+                  ></img>
+                  {row.name}
+                </TableCell>
+                <TableCell style={{ textTransform: "uppercase" }}>
+                  {row.symbol}
+                </TableCell>
+                <TableCell>{row.current_price}</TableCell>
+                <TableCell>
+                  {row.total_volume
+                    ? `$${row.total_volume.toLocaleString()}`
+                    : " "}
+                </TableCell>
+                <TableCell>
+                  {row.circulating_supply
+                    ? row.circulating_supply.toLocaleString()
+                    : " "}
+                </TableCell>
+                <TableCell>
+                  {row.market_cap ? `$${row.market_cap.toLocaleString()}` : " "}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </TableContainer>
-      <Grid container justify="center" style={{ marginTop: 10, paddingBottom: 10, maxWidth: "95%"}}>
+      <Grid
+        container
+        justify="center"
+        style={{ marginTop: 10, paddingBottom: 10, maxWidth: "95%" }}
+      >
         <Pagination
           onPageChange={changePage}
           activePage={cpage}
           totalPages={num}
-          ellipsisItem={null}
         />
       </Grid>
     </Paper>
-  )
+  );
 }
