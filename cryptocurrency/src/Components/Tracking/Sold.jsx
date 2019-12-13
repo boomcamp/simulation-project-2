8543d5ddd4f5cb5bdd9dd82fd92071960bf60108
serra-vp <vincent.serra@boom.camp>
2019-12-13 18:04:31 +0800

    with sell but with bugs

--- cryptocurrency/src/Components/Tracking/Sold.jsx
+++ cryptocurrency/src/Components/Tracking/Sold.jsx
@@ -4,7 +4,7 @@ import { Button } from '@material-ui/core'
 function Sold(props) {
   console.log(props)
   return (
-    <Button disabled={true} color="secondary">Sold for ${props.transactionData.sellPrice}(${props.transactionData.earned})</Button>
+    <Button disabled={true} variant="contained" color="secondary">Sold for ${props.transactionData.sellPrice}(${props.transactionData.earned})</Button>
   )
 }
 
