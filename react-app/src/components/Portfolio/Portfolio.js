33101a71ba52bc50391cc2d27dd1fd45851fa862
lyzamaemirabete <lyza.mirabete@boom.camp>
2020-03-04 17:36:39 +0800

    updated display error

--- react-app/src/components/Portfolio/Portfolio.js
+++ react-app/src/components/Portfolio/Portfolio.js
@@ -1,11 +1,20 @@
-import React, { useEffect } from "react";
+import React, {
+  useEffect
+} from "react";
 import axios from "axios";
 import MaterialTable from "material-table";
-import { NavLink, useParams } from "react-router-dom";
-import { Search } from "semantic-ui-react";
+import {
+  NavLink,
+  useParams
+} from "react-router-dom";
+import {
+  Search
+} from "semantic-ui-react";
 
 export default function MaterialTableDemo() {
-  const { id } = useParams();
+  const {
+    id
+  } = useParams();
 
   const [state, setState] = React.useState({
     data: []
@@ -15,8 +24,8 @@ export default function MaterialTableDemo() {
   const [coinLs, setCoinLs] = React.useState({});
 
   const reducer = objs => {
-    return objs.reduce(function(o, cur) {
-      var occurs = o.reduce(function(n, item, i) {
+    return objs.reduce(function (o, cur) {
+      var occurs = o.reduce(function (n, item, i) {
         return item.coinId === cur.coinId ? i : n;
       }, -1);
       if (occurs >= 0) {
@@ -52,8 +61,8 @@ export default function MaterialTableDemo() {
       var output = reducer(buyData);
       var outputAfterSell = reducer(sellData);
       var all = [...output, ...outputAfterSell];
-      var overall = all.reduce(function(o, cur) {
-        var occurs = o.reduce(function(n, item, i) {
+      var overall = all.reduce(function (o, cur) {
+        var occurs = o.reduce(function (n, item, i) {
           return item.coinId === cur.coinId ? i : n;
         }, -1);
         if (occurs >= 0) {
@@ -90,12 +99,15 @@ export default function MaterialTableDemo() {
       const pricesWs = new WebSocket(
         `wss://ws.coincap.io/prices?assets=${coins}`
       );
-      pricesWs.onmessage = function(msg) {
+      pricesWs.onmessage = function (msg) {
         Object.keys(JSON.parse(msg.data)).forEach(e => {
           if (JSON.parse(msg.data)[`${e}`] !== "")
             coinList[e] = JSON.parse(msg.data)[`${e}`];
           setCoinLs(coinList);
-          setState({ ...state, data: overall });
+          setState({
+            ...state,
+            data: overall
+          });
         });
       };
     });
@@ -105,156 +117,201 @@ export default function MaterialTableDemo() {
     return (Number(priceSold) - Number(current)) / Number(current);
   };
 
-  return (
-    <div style={{ overflowY: "scroll" }}>
-      <MaterialTable
-        style={{
-          minHeight: "50vh",
-          maxHeight: "50vh",
-          padding: "0px 20px 20px 20px",
-          borderRadius: "0px",
-          boxShadow: "none"
-        }}
-        title="Invested Coins"
-        columns={[
-          {
-            title: "Coin",
-            headerStyle: {
-              height: 10,
-              fontWeight: "bold"
-            },
-            render: rowData => (
-              <NavLink to={`/history/${rowData.coinId}`}>
-                <div>
-                  <img className="logo" src={rowData.img} alt="Img" />
-                  <p style={{ textTransform: "uppercase" }}>{rowData.coin}</p>
-                </div>
-              </NavLink>
-            )
+  return ( <
+    div style = {
+      {
+        overflowY: "scroll"
+      }
+    } >
+    <
+    MaterialTable style = {
+      {
+        minHeight: "50vh",
+        maxHeight: "50vh",
+        padding: "0px 20px 20px 20px",
+        borderRadius: "0px",
+        boxShadow: "none"
+      }
+    }
+    title = "Invested Coins"
+    columns = {
+      [{
+          title: "Coin",
+          headerStyle: {
+            height: 10,
+            fontWeight: "bold"
           },
-          {
-            title: "Last Price (USD)",
-            headerStyle: {
-              height: 10,
-              fontWeight: "bold"
-            },
-            field: "",
-            render: rowData => {
-              return <p>{(+coinLs[rowData.coinId]).toFixed(4)}</p>;
+          render: rowData => ( <
+            NavLink to = {
+              `/history/${rowData.coinId}`
+            } >
+            <
+            div >
+            <
+            img className = "logo"
+            src = {
+              rowData.img
             }
+            alt = "Img" / >
+            <
+            p style = {
+              {
+                textTransform: "uppercase"
+              }
+            } > {
+              rowData.coin
+            } < /p> <
+            /div> <
+            /NavLink>
+          )
+        },
+        {
+          title: "Last Price (USD)",
+          headerStyle: {
+            height: 10,
+            fontWeight: "bold"
           },
-          {
-            title: "Average Price (USD)",
-            headerStyle: {
-              height: 10,
-              fontWeight: "bold"
-            },
-            render: rowData => {
-              return <p>rowData.currentPrice.toFixed(4)}</p>;
-            },
-            type: "numeric"
-          },
-          {
-            title: "Coin Quantity",
-            headerStyle: {
-              height: 10,
-              fontWeight: "bold"
-            },
-            headerStyle: {
-              height: 10,
-              fontWeight: "bold"
-            },
-            type: "numeric",
-            render: rowData => {
-              return <p>{rowData.coinQuantity.toFixed(4)}</p>;
-            }
-          },
-          {
-            title: "Total Cost (USD)",
-            headerStyle: {
-              height: 10,
-              fontWeight: "bold"
-            },
-            type: "numeric",
-            render: rowData => {
-              return <p>{rowData.totalAmount.toFixed(2)}</p>;
-            }
-          },
-          {
-            title: "Market Value",
-            headerStyle: {
-              height: 10,
-              fontWeight: "bold"
-            },
-
-            type: "numeric",
-            render: rowData => {
-              var gainLosss = gainLoss(
-                coinLs[rowData.coinId],
-                rowData.currentPrice
-              );
-              var onePer = (Number(gainLosss) - Number(0.01)) * 100;
-              var percentageProfit = Number(gainLosss) + Number(onePer);
-              var profit =
-                Number(rowData.totalAmount) *
-                (Number(percentageProfit) / Number(100));
-              var marketVal = Number(rowData.totalAmount) + Number(profit);
-              return <p>{(+marketVal).toFixed(2)}</p>;
-            }
-          },
-          {
-            title: "Est. Profit (USD)",
-            headerStyle: {
-              height: 10,
-              fontWeight: "bold"
-            },
-            type: "numeric",
-            render: rowData => {
-              var gainLosss = gainLoss(
-                coinLs[rowData.coinId],
-                rowData.currentPrice
-              );
-              var onePer = (Number(gainLosss) - Number(0.01)) * 100;
-              var percentageProfit = Number(gainLosss) + Number(onePer);
-              var profit =
-                Number(rowData.totalAmount) *
-                (Number(percentageProfit) / Number(100));
-              return (
-                <p className={profit > 0 ? "green" : "red"}>
-                  {(+profit).toFixed(2)}
-                </p>
-              );
-            }
-          },
-          {
-            title: "% Profit",
-            headerStyle: {
-              height: 10,
-              fontWeight: "bold"
-            },
-            type: "numeric",
-            render: rowData => {
-              var gainLosss = gainLoss(
-                coinLs[rowData.coinId],
-                rowData.currentPrice
-              );
-              var onePer = (Number(gainLosss) - Number(0.01)) * 100;
-              var percentageProfit = Number(gainLosss) + Number(onePer);
-              return (
-                <p className={percentageProfit > 0 ? "green" : "red"}>
-                  {(+percentageProfit).toFixed(4)}%
-                </p>
-              );
-            }
+          field: "",
+          render: rowData => {
+            return <p > {
+              (+coinLs[rowData.coinId]).toFixed(4)
+            } < /p>;
           }
-        ]}
-        data={state.data}
-        editable={{}}
-        options={{
-          paging: false,
-          search: false
-        }}
-      />
-    </div>
+        },
+        {
+          title: "Average Price (USD)",
+          headerStyle: {
+            height: 10,
+            fontWeight: "bold"
+          },
+          render: rowData => {
+            return <p > {
+              rowData.currentPrice.toFixed(4)
+            } < /p>;
+          },
+          type: "numeric"
+        },
+        {
+          title: "Coin Quantity",
+          headerStyle: {
+            height: 10,
+            fontWeight: "bold"
+          },
+          headerStyle: {
+            height: 10,
+            fontWeight: "bold"
+          },
+          type: "numeric",
+          render: rowData => {
+            return <p > {
+              rowData.coinQuantity.toFixed(4)
+            } < /p>;
+          }
+        },
+        {
+          title: "Total Cost (USD)",
+          headerStyle: {
+            height: 10,
+            fontWeight: "bold"
+          },
+          type: "numeric",
+          render: rowData => {
+            return <p > {
+              rowData.totalAmount.toFixed(2)
+            } < /p>;
+          }
+        },
+        {
+          title: "Market Value",
+          headerStyle: {
+            height: 10,
+            fontWeight: "bold"
+          },
+
+          type: "numeric",
+          render: rowData => {
+            var gainLosss = gainLoss(
+              coinLs[rowData.coinId],
+              rowData.currentPrice
+            );
+            var onePer = (Number(gainLosss) - Number(0.01)) * 100;
+            var percentageProfit = Number(gainLosss) + Number(onePer);
+            var profit =
+              Number(rowData.totalAmount) *
+              (Number(percentageProfit) / Number(100));
+            var marketVal = Number(rowData.totalAmount) + Number(profit);
+            return <p > {
+              (+marketVal).toFixed(2)
+            } < /p>;
+          }
+        },
+        {
+          title: "Est. Profit (USD)",
+          headerStyle: {
+            height: 10,
+            fontWeight: "bold"
+          },
+          type: "numeric",
+          render: rowData => {
+            var gainLosss = gainLoss(
+              coinLs[rowData.coinId],
+              rowData.currentPrice
+            );
+            var onePer = (Number(gainLosss) - Number(0.01)) * 100;
+            var percentageProfit = Number(gainLosss) + Number(onePer);
+            var profit =
+              Number(rowData.totalAmount) *
+              (Number(percentageProfit) / Number(100));
+            return ( <
+              p className = {
+                profit > 0 ? "green" : "red"
+              } > {
+                (+profit).toFixed(2)
+              } <
+              /p>
+            );
+          }
+        },
+        {
+          title: "% Profit",
+          headerStyle: {
+            height: 10,
+            fontWeight: "bold"
+          },
+          type: "numeric",
+          render: rowData => {
+            var gainLosss = gainLoss(
+              coinLs[rowData.coinId],
+              rowData.currentPrice
+            );
+            var onePer = (Number(gainLosss) - Number(0.01)) * 100;
+            var percentageProfit = Number(gainLosss) + Number(onePer);
+            return ( <
+              p className = {
+                percentageProfit > 0 ? "green" : "red"
+              } > {
+                (+percentageProfit).toFixed(4)
+              } %
+              <
+              /p>
+            );
+          }
+        }
+      ]
+    }
+    data = {
+      state.data
+    }
+    editable = {
+      {}
+    }
+    options = {
+      {
+        paging: false,
+        search: false
+      }
+    }
+    /> <
+    /div>
   );
 }
